e53327b008aa0e27840e272a00836c5030286c24
Damian Boni <damianboni@192.168.1.106>
2023-05-26 02:11:25 +0100

    init

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-200">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @vite(['resources/css/app.css'])
+    @routes
+</head>
+<body class="font-sans antialiased leading-none text-gray-800">
+
+@inertia
+
+</body>
+</html>
